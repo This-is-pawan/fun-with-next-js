e400f51d39065a71a52e8228426e7a12615672f0
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-01-11 17:27:57 +0530

    Update page.tsx

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -3,7 +3,8 @@
 export default function page() {
   return (
     <div>
-      <h1 className="p-3">About page  </h1> <p>This is a about page of nextjs 🧪</p>
+      <h1 className="p-3">About page  </h1> <p>This is a about page of nextjs experiment and write the logic 
+      </p>
     </div>
   )
 }
